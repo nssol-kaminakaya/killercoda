4d47039bc5b8108cd4fdb9e9c5ebe3f6f8d0612a
kkmnky <kaminakaya.ken.7s5@jp.nssol.nipponsteel.com>
2022-09-30 10:58:13 +0900

    intro修正

--- distributed-tracing-handson/3-auto-instrumentation/setup.sh
+++ distributed-tracing-handson/3-auto-instrumentation/setup.sh
@@ -1,2 +1,5 @@
 touch /var/dont-need-this.png
-git clone https://github.com/microservices-demo/microservices-demo
+git clone https://github.com/nssol-kaminakaya/otel-demo
+cd otel-demo
+docker-compose pull
+docker-compose build
